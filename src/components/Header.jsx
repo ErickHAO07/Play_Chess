a46f2e0c7f5cc3d180dfa978a2b809e7a9daa94f
ErickHAO07 <henri322005@gmail.com>
2026-09-09 06:40:43 -0300

    Design of the hero section and footer for Android

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 function Header() {
     return (
         <header className="bg-Theme  w-full h-10">
-            <nav className="text-white text-fluid-subtitle w-[97%] h-full flex gap-10 justify-start items-center">
+            <nav className="px-2 text-white text-fluid-subtitle w-[97%] h-full flex gap-10 justify-start items-center">
                 <a href="">Logar</a>
                 <a href="">Play</a>
                 <a href="">Home</a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,32 +8,37 @@ function Home() {
     return (
 
 
-        <div className="grid grid-cols-1 gap-10 place-items-center  bg-transparent w-full min-h-dvh mt-6">
+        <div className="grid grid-cols-1 gap-40 place-items-center  bg-transparent w-full min-h-dvh mt-6">
 
-            <div className='flex flex-col items-center w-full text-white '>
+            <div className='flex flex-col gap-0 items-center w-full text-white '>
 
                 <ChessPlay />
                 <h1 className='text-fluid-title font-trebuchet font-bold mt-auto ' >Play Chess with us!</h1>
-                <p className=' font-arial w-3/5 text-fluid-text  '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex doloribus adipisc</p>
-                <input type="button" value="Start" className=' w-3/7 h-10 bg-botton-theme self-center rounded-md text-fluid-subtitle font-helvetica font-bold text-white ' />
+                <p className=' font-arial text-fluid-text  '>Join in one of the largest comunitys around the world </p>
+                <input type="button" value="Start" className=' px-7 py-1 mt-1 bg-botton-theme rounded-md text-fluid-buttons font-helvetica font-bold text-white ' />
             </div>
 
-            <div className='flex flex-col items-center '>
-                <h2 className='text-fluid-title text-white'>Chalenge yourself Against our AI</h2>
+            <div className='flex flex-col items-center text-white '>
 
+
+                <h2 className='text-fluid-title font-trebuchet font-bold'>The best AI to play with</h2>
+                <p className=' font-arial text-fluid-text  '>Try yourself against our AI chess model</p>
                 <img className=' '
                     src={Rooke}
                     alt="Digital chess rook piece "
                     width={200}
                     height={200} />
+
+
+
             </div>
 
 
-            <div className='flex flex-col w-full items-center'>
+            <div className='flex flex-col w-full items-center text-white'>
 
 
-                <h2 className='text-3xl text-white '>Improve your Chess skills with us</h2>
-                <p className='  '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, mollitia blanditiis. Ad et expedita id eum itaque eius odit nostrum culpa molestiae quis. Accusantium quisquam qui adipisci unde, reprehenderit et.</p>
+                <h2 className='text-fluid-title font-trebuchet font-bold  '>Improve your Chess skills</h2>
+                <p className='font-arial text-fluid-text  '>move your chess skills to the next level</p>
 
                 <img className=''
                     src={ChessLevels}
@@ -42,7 +47,7 @@ function Home() {
                     height={100} />
 
 
-                <input className='w-3/7 text-2xl text-white bg-blue-600 self-center' type="button" value="Start practice" />
+                <input type="button" value="Start practice" className='px-7 py-1 bg-botton-theme mt-1 rounded-md text-fluid-buttons font-helvetica font-bold text-white' />
             </div>
 
 
